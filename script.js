80082bf38c24604cc1980db01330bdb40096b50d
marvelade <marveladeadewuyi@gmail.com>
2025-09-11 23:14:29 +0100

    Added the order funtion

--- script.js
+++ script.js
@@ -46,29 +46,98 @@ document.querySelectorAll('.side-bar a').forEach(link => {
     link.addEventListener('click', hideSidebar);
 });
 
-function calculatePrice () {
+function calculatePrice2 () {
   const quantity2 = document.getElementById('quantity2').value;
-  const price = document.getElementById('price');
-  const pricee = price.textContent
-  let a = Number(pricee)
+  const amt = document.querySelector('.amt');
+  const amtt = amt.textContent
+  let a = Number(amtt)
   total = quantity2 * a
 
- const abc = document.getElementById('s');
+ const submit = document.getElementById('submit2');
 
- if (abc.innerHTML === 'Confirm Order') {
-   abc.style.display = 'none'
-   document.querySelector('.order').style.display = 'none'
+ if (submit.innerHTML === 'Confirm Order') {
+   submit.style.display = 'none'
 
-  document.getElementById('confirmText').innerHTML = 'Your Order is successful 🥰😊'
+  document.querySelector('.confirmText').innerHTML = 'Your Order is successful 🥰😊'
+
+  document.querySelector('.totalPrice').style.display = 'none'
   setTimeout(() => location.reload(), 3000);
 
  } else {
-   document.getElementById('totalPrice').innerHTML = `Your total price is $${total}`
-  abc.innerHTML = 'Confirm Order';
+   document.querySelector('.totalPrice').innerHTML = `Your total price is $${total}`
+  submit.innerHTML = 'Confirm Order';
   
  }
 }
 
-function confirmOrder() {
-  document.querySelector('.d').style.display = 'none'
+function calculatePrice1 () {
+  const quantity1 = document.getElementById('quantity1').value;
+  const amt = document.querySelector('.amt1');
+  const amtt = amt.textContent
+  let a = Number(amtt)
+  total = quantity1 * a
+
+   const submit = document.getElementById('submit1');
+
+ if (submit.innerHTML === 'Confirm Order') {
+   submit.style.display = 'none'
+
+  document.querySelector('.confirmText1').innerHTML = 'Your Order is successful 🥰😊'
+
+  document.querySelector('.totalPrice1').style.display = 'none'
+  setTimeout(() => location.reload(), 3000);
+
+ } else {
+   document.querySelector('.totalPrice1').innerHTML = `Your total price is $${total}`
+  submit.innerHTML = 'Confirm Order';
+  
+ }
+}
+
+function calculatePrice3 () {
+  const quantity = document.getElementById('quantity3').value;
+  const amt = document.querySelector('.amt3');
+  const amtt = amt.textContent
+  let a = Number(amtt)
+  total = quantity * a
+
+   const submit = document.getElementById('submit3');
+
+ if (submit.innerHTML === 'Confirm Order') {
+   submit.style.display = 'none'
+
+  document.querySelector('.confirmText3').innerHTML = 'Your Order is successful 🥰😊'
+
+  document.querySelector('.totalPrice3').style.display = 'none'
+  setTimeout(() => location.reload(), 3000);
+
+ } else {
+   document.querySelector('.totalPrice3').innerHTML = `Your total price is $${total}`
+  submit.innerHTML = 'Confirm Order';
+  
+ }
+}
+
+function calculatePrice4 () {
+  const quantity = document.getElementById('quantity4').value;
+  const amt = document.querySelector('.amt4');
+  const amtt = amt.textContent
+  let a = Number(amtt)
+  total = quantity * a
+
+   const submit = document.getElementById('submit4');
+
+ if (submit.innerHTML === 'Confirm Order') {
+   submit.style.display = 'none'
+
+  document.querySelector('.confirmText4').innerHTML = 'Your Order is successful 🥰😊'
+
+  document.querySelector('.totalPrice4').style.display = 'none'
+  setTimeout(() => location.reload(), 3000);
+
+ } else {
+   document.querySelector('.totalPrice4').innerHTML = `Your total price is $${total}`
+  submit.innerHTML = 'Confirm Order';
+  
+ }
 }
